5f0ab9baaf678b643fb515de38471b222631689d
Welpike <106405084+Welpike@users.noreply.github.com>
2023-03-29 09:42:57 +0000

    add datas+some updates

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Dashboard for CSV datas (Buildings protected as Historical Monuments (archives), France). Source : https://www.data.gouv.fr/fr/datasets/liste-des-immeubles-proteges-au-titre-des-monuments-historiques-archives/
 
 Lang : Fr
+
+Warning : the `./datas/db.csv` is really big (34 849 Ko).
--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
 import folium
 import os
 import webbrowser
-from urllib.request import urlopen
 
 def build_map(filepath):
     with open(filepath, 'r') as file:
         for line in file.readlines():
-            print(file)
+            line = line.strip().split(';')
+            print(line)
 
-build_map(urlopen("https://www.data.gouv.fr/fr/datasets/r/86a7f194-182e-4be3-ac5a-082d612082fe"))
+def build_htmlfile(filepath):
+    with open(filepath, "w+") as file:
+        file.write(
+            """
+                
+            """
+        )
+
+"""
+pour les 2 cartes :
+    - propriété privée
+    - propriété communale
+"""
+
+build_map('datas/db.csv')
