2a00c74a1a4383b7621b9b765a65189444c13a57
Welpike <106405084+Welpike@users.noreply.github.com>
2023-04-03 09:58:14 +0000

    some updates (main)

--- src/main.py
+++ src/main.py
@@ -1,45 +1,34 @@
 import os
 import webbrowser
 import folium
-"""
-def build_map(filepath):
-    with open(filepath, 'r') as file:
-        for line in file :
 
-        for line in file.readlines():
-            line = line.strip().split(';')
-            print(line)
+def build_map(filepath):
+    data_list=[]  # list of all building in Angers, France
+    town_map=folium.Map(location=[47.471646,-0.551846],zoom_start=13)
+    private_map=folium.Map(location=[47.471646,-0.551846],zoom_start=13)
+    with open(filepath,'r',encoding='utf8') as file:
+        for line in file:
+            line_data=line.strip().split(";")
+            if line_data[0]=="Angers" :
+                print(line_data)
+                data_list.append(line_data)
+        private=[]
+        town=[]
+        for i in data_list:
+            if i[12]=="propriété privée":
+                private.append(i)
+            elif i[12]=="propriété de la commune":
+                town.append(i)
+    town_map.save('./templates/embed/town.html')
+    private_map.save('./templates/embed/private.html')
 
 def build_htmlfile(filepath):
     with open(filepath, "w+") as file:
         file.write(
             """
                 
-""")
+            """
+        )
+
+
 build_map('datas/db.csv')
-
-"""
-
-def fabricationCarte(fichier):
-    liste=[]
-    carte_town=folium.Map(location=[47.471646,-0.551846],zoom_start=13)
-    carte_private=folium.Map(location=[47.471646,-0.551846],zoom_start=13)
-    
-    f1=open(fichier,'r',encoding='utf8')
-    for ligne in f1:
-        ligne_data=ligne.strip().split(";")
-        if ligne_data[0]=="Angers" :
-            print(ligne_data)
-            liste.append(ligne_data)
-    private=[]
-    town=[]
-    for i in liste:
-        if i[12]=="propriété privée":
-            private.append(i)
-        elif i[12]=="propriété de la commune":
-            town.append(i)
-    f1.close()
-    carte_town.save('./templates/embed/town.html')
-    carte_private.save('./templates/embed/private.html')
-fabricationCarte("datas/db.csv")
-
