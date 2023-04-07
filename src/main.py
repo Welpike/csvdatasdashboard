195419a6a35cfda6e4d2aba44864296902165d0d
Raphael06b <129053755+Raphael06b@users.noreply.github.com>
2023-04-07 15:08:19 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,49 +6,50 @@ def build_map(filepath):
     data_list=[]  # list of all building in Angers, France
     town_map=folium.Map(location=[47.471646,-0.551846],zoom_start=13)
     private_map=folium.Map(location=[47.471646,-0.551846],zoom_start=13)
+    private=[]
+    town=[]
+    nb_private=0
+    nb_town=0
     with open(filepath,'r',encoding='utf8') as file:
         for line in file:
             line_data=line.strip().split(";")
             if line_data[0]=="Angers" :
+                coor=line_data[-1].split(',')
                 print(line_data)
                 data_list.append(line_data)
-        private=[]
-        town=[]
-        for i in data_list:
-            if i[12]=="propriété privée":
-                private.append(i)
-            elif i[12]=="propriété de la commune":
-                town.append(i)
+                if line_data[12]=='propriété de la commune':  
+                    nb_town+=1
+                    town.append(line_data[0])                
+                    folium.Marker([float(coor[0]),float(coor[1])],popup=line_data[6],icon=folium.Icon(color='red')).add_to(town_map)
+                elif line_data[12]=='propriété privée':
+                    nb_private+=1
+                    private.append(line_data[0])  
+                    folium.Marker([float(line_data[coor[0]]),float(coor[1])],popup=line_data[6],icon=folium.Icon(color='red')).add_to(private_map)
+        
     town_map.save('./templates/embed/town.html')
     private_map.save('./templates/embed/private.html')
 
-def build_htmlfile(filepath):
+    info={
+        "town": {
+            "nb": nb_town,
+       	    "names": town
+        },
+        "private": {
+            "nb": nb_private,
+       	    "names": private
+        }
+    }
+    return(info)
+
+def build_htmlfile(filepath, infos: dict):
     with open(filepath, "w+") as file:
         file.write(
             """
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Buildings protected as Historical Monuments (archives), France - CSVDATASDASHBOARD</title>
-                <link rel="stylesheet" href="../static/main.css">
-            </head>
-            <body>
-                <h1>FRANCE : Buildings protected as Historical Monuments (archives)</h1>
-                <p>Map :</p>
-                <iframe id="map_iframe" src="" height="700" width="1000"></iframe>
-
-                <a id="change_map_btn"></a>
-
-                <script src="../static/main.js"></script>
-            </body>
-            </html>
+                
             """
         )
 
 
-build_map('datas/db.csv')
-build_htmlfile('templates/index.html')
+infos=build_map('datas/db.csv')
+build_htmlfile("templates/index.html", infos)
 w_open(getcwd()+"/templates/index.html")
